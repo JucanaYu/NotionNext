5b80233c12d1be3efc4d211b7900151128c5aac7
JucanaYu <39491923+JucanaYu@users.noreply.github.com>
2023-12-21 15:43:27 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -10,12 +10,12 @@ const BLOG = {
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: 2021, // e.g if leave this empty, current year will be used.
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'auto', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
-  APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
+  APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [21, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'JucanaYu', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '研1，网络安全硬件（芯片）学习者', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '爱吃美食的人', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://jucanayu.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '学习记录,博客,blog', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
